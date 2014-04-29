22cf1fadd045c1a09b1ec039a5d458a8ee79b730
Amiruz <amirajady@hotmail.com>
2014-04-30 01:04:04 +0200

    Update EndMenu.cs
    Added a GUIbox

--- Unity/Super Junky World/Assets/Scripts/EndMenu.cs
+++ Unity/Super Junky World/Assets/Scripts/EndMenu.cs
@@ -1,7 +1,13 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EndMenu : MonoBehaviour {
 
-	//
+	void OnGUI () {
+	
+		GUI.Box(new Rect(10,10,100,90), "End score: ");
+
+	
+		}
 }
+
