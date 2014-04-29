1a83aa1466e0f2095dedc3d13b2682c6afe66b1a
Amiruz <amirajady@hotmail.com>
2014-04-30 01:40:17 +0200

    Update EndMenu.cs
    Added Q as a Exit button

--- Unity/Super Junky World/Assets/Scripts/EndMenu.cs
+++ Unity/Super Junky World/Assets/Scripts/EndMenu.cs
@@ -1,13 +1,16 @@
 using UnityEngine;
 using System.Collections;
 
-public class EndMenu : MonoBehaviour {
-
-	void OnGUI () {
+public class EndMenu : MonoBehaviour 
+{	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Q))
+			Application.Quit();	
+	}
 	
+	void OnGUI () 
+	{
 		GUI.Box(new Rect(10,10,100,90), "End score: ");
-
-	
-		}
+	}
 }
-
