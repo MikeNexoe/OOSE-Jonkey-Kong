fee3ae1e2c118bcc2bdf68d954ea8082e529f09f
MikeNexoe <mikenexoe@hotmail.com>
2014-04-29 22:17:10 +0200

    Scene1
    Update to first level and bullet

---
+++ Unity/Super Junky World/Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= new Vector3 (0.02f, 0, 0);
+	}
+
+	void OnCollisionEnter(Collision other)
+	{
+		if (other.gameObject.name == "Junky") 
+			Destroy (gameObject);
+	}
+}
