c51b8d9543850f545ea2ab17a088acea64439623
Amiruz <amirajady@hotmail.com>
2014-04-30 01:30:51 +0200

    Update controlls.cs
    Added a Q as a exit button

--- Unity/Super Junky World/Assets/Scripts/controlls.cs
+++ Unity/Super Junky World/Assets/Scripts/controlls.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class controlls : MonoBehaviour {
@@ -6,11 +6,13 @@ public class controlls : MonoBehaviour {
 	void OnGUI () {
 		// Make a background box
 
-		GUI.Box(new Rect(10,20,300,100), "Press a for left");
-		GUI.Box(new Rect(10,40,300,100), "Press d for right");
+		GUI.Box(new Rect(10,20,300,100), "Press A for left");
+		GUI.Box(new Rect(10,40,300,100), "Press D for right");
 		GUI.Box(new Rect(10,60,300,100), "Press space for jump");
 		GUI.Box(new Rect(10,80,300,100), "Press ctrl for shrink");
-		GUI.Box(new Rect(10,100,300,100), "Press m for Menu"); {
+		GUI.Box(new Rect(10,100,300,100), "Press M for Menu");
+		GUI.Box(new Rect(10,120,300,100), "Press Q for EXITE game"); {
+			
 		if (Input.GetKeyDown(KeyCode.M)){
 			Application.LoadLevel("Menu"); }
 
