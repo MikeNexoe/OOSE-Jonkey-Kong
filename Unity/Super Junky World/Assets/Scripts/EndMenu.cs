a6a16f6f43ba2e563a04105415ffae70abae7b6b
Amiruz <amirajady@hotmail.com>
2014-04-30 02:16:36 +0200

    Update EndMenu.cs
    same as previous

--- Unity/Super Junky World/Assets/Scripts/EndMenu.cs
+++ Unity/Super Junky World/Assets/Scripts/EndMenu.cs
@@ -1,28 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class EndMenu : MonoBehaviour 
+public class EndMenu : MonoBehaviour
 {	
-	void Update () 
-	{
-		if (Input.GetKeyDown (KeyCode.Q))
-			Application.Quit();	
-	}
-			
-public void AddPoints (float points) {
-    
-		score+=points;
-		
-    Debug.Log("Score: " + score);
-    	}
-	
-	
-	
-	void OnGUI () 
-	{
-		GUI.Box(new Rect(10,10,100,90), "End score: ");
-	}
+void Update ()
+{
+	if (Input.GetKeyDown (KeyCode.Q))
+		Application.Quit();	
+}
 
-	}
+void OnGUI ()
+{
+	GUI.Box(new Rect(10,10,100,90));
+		Application.LoadLevel ("HighScore");
+}
 
-
+}
